3b8b8b7746adaed2ad4dce2455694b32984dd3b1
jbull477 <jbull477@gmail.com>
2015-04-26 14:31:28 -0400

    search improvements

--- app/models/apple.rb
+++ app/models/apple.rb
@@ -4,8 +4,8 @@ class Apple < ActiveRecord::Base
   validates :diameter, :numericality => {:greater_than => 0.0}
   validates :height, :numericality => {:greater_than => 0.0}
 
-  def self.search(search)
-    return all if search.blank?
-    where('LOWER(name) LIKE LOWER(?)', "%#{search}%")
+  def self.search(query)
+    return all if query.blank?
+    where('LOWER(name) LIKE LOWER(?)', "%#{query}%")
   end
 end
